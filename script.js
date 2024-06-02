7277b99805d6e8a74d9c4c2ffa7d03567ddd208c
janvijayant5 <janvijayant5@gmail.com>
2024-06-02 19:28:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const display = document.querySelector('.display');
+  const buttons = document.querySelectorAll('button');
+  const specialChars = ['%', '*', '/', '-', '+', '='];
+  let output = '';
+
+  const calculate = (btnValue) => {
+      if (btnValue === '=' && output !== '') {
+          try {
+              output = eval(output.replace('%', '/100'));
+          } catch {
+              output = 'Error';
+          }
+      } else if (btnValue === 'AC') {
+          output = '';
+      } else if (btnValue === 'DEL') {
+          output = output.slice(0, -1);
+      } else {
+          if (output === '' && specialChars.includes(btnValue)) return;
+          output += btnValue;
+      }
+      display.value = output;
+  };
+
+  buttons.forEach((button) => {
+      button.addEventListener('click', (e) => calculate(e.target.dataset.value));
+  });
+});
